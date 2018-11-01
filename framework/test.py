27a01438a83308fe46b44497012a955341c19fd7
Elisabetta Manca <elisabetta.manca@cern.ch>
2018-11-01 17:22:15 +0100

    dummy test with 2 modules

--- framework/test.py
+++ framework/test.py
@@ -1,6 +1,5 @@
 from RDFprocessor import *
 
-print "i am here!"
 
-p = RDFprocessor(outputFiles = 'test.root', inputFiles ='/scratch/emanca/WMass/NanoDevelopment/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/wmass/test80X_NANO_Skim.root', modules=[module()], cores=20)
+p = RDFprocessor(outputFiles = 'test.root', inputFiles ='/scratch/emanca/WMass/NanoDevelopment/CMSSW_10_2_6/src/PhysicsTools/NanoAODTools/python/postprocessing/wmass/test80X_NANO_Skim.root', modules=[module(),module2()], cores=20)
 p.run()
